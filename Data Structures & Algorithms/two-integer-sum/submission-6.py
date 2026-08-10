e2a073b8f24025d5ef3757502adb17d5236359cf
Umar Ali <mali232@illinois.edu>
2026-08-10 18:40:37 -0500

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        target_map = {}
+
+        for i in range(len(nums)):
+            goal = target - nums[i]
+
+            if goal in target_map:
+                return [target_map[goal], i]
+
+            target_map[nums[i]] = i
+        
+
+            
+
+     
