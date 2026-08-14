277298b2b85196434cd51c0aec442e8bdc3b494b
Umar Ali <mali232@illinois.edu>
2026-08-14 00:38:49 -0500

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        
+
+        s = s.replace(" ", "")
+        s = s.upper()
+
+        right = len(s) - 1
+
+        while left < right:
+            while left < right and s[left].isalnum() == False:
+                left += 1
+            while left < right and s[right].isalnum() == False:
+                right -= 1
+
+            if s[left] != s[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+    
+        return True
